a43bc534f0d65764f7ccd83932a24afd84a3f7a7
NEVESENIN <nevesenin@cpan.org>
2011-01-12 17:18:16 +0000

    Import https://cpan.metacpan.org/authors/id/N/NE/NEVESENIN/CSS-Packer-0.05_01.tar.gz

--- lib/CSS/Packer.pm
+++ lib/CSS/Packer.pm
@@ -1,16 +1,16 @@
 package CSS::Packer;
 
-use 5.008009;
+use 5.008008;
 use warnings;
 use strict;
 use Carp;
 use Regexp::RegGrp;
 
-our $VERSION        = '0.0401';
+our $VERSION        = '0.05_01';
 
 our $DICTIONARY     = {
-    'STRING1'   => qr~"(?>(?:(?>[^"\\]+)|\\.|\\"|\\\s)*)"~,
-    'STRING2'   => qr~'(?>(?:(?>[^'\\]+)|\\.|\\'|\\\s)*)'~
+    STRING1 => qr~"(?>(?:(?>[^"\\]+)|\\.|\\"|\\\s)*)"~,
+    STRING2 => qr~'(?>(?:(?>[^'\\]+)|\\.|\\'|\\\s)*)'~
 };
 
 our $WHITESPACES    = '\s+';
@@ -316,7 +316,7 @@ CSS::Packer - Another CSS minifier
 
 =head1 VERSION
 
-Version 0.0401
+Version 0.05_01
 
 =head1 DESCRIPTION
 
